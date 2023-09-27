63181837f182c67e4e6c05dbd7a8c4c66b8963d2
GunjanSaras <gunjansaraswat0701@gmail.com>
2023-09-27 07:44:00 -0400

    Milestone 2: Game Intro

---
+++ Game.py
@@ -0,0 +1,15 @@
+#game introduction 
+print("Welcome to the Text Adventure Game!")
+print("In a world shrouded in mystery and danger, you stand at the crossroads of destiny.")
+print("As you embark on this thrilling journey, you must choose your path wisely.")
+print("Two remarkable roles await you:")
+print("1. Wizard - A wielder of arcane powers, mastering intelligence and magic.")
+print("2. Barbarian - A mighty warrior, known for unmatched strength and valor.")
+
+#Quest 
+print("GET READY FOR THE QUEST!")
+print("The treasure is hidden in the Amazon Rainforest. Among the DeAdLiEsT animals!")
+print("Take it upon you to find it while managing to survive in this dangerous rainforest!")
+print("You will be tested on all of your attributes and accordingly you will face three challenges!")
+print("In order to win, you have to win all the three challenges! ")
+print("Failure in any of the challenge will lead to an overall failure in the game.")
